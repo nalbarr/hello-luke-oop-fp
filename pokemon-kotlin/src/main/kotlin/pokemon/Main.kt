dbbcea1c98e2d2149f9e634da6fc392a946319db
nalbarr <nalbarracin@gmail.com>
2019-04-19 21:58:19 -0400

    NA.  Initial seed.  Create subprojects per java, kotlin, and scala

---
+++ pokemon-kotlin/src/main/kotlin/pokemon/Main.kt
@@ -0,0 +1,13 @@
+package pokemon
+
+class Main {
+    val greeting: String
+        get() {
+            return "Hello world."
+        }
+}
+
+fun main(args: Array<String>) {
+    println(Main().greeting)
+}
+~
---
+++ pokemon-scala/src/main/scala/pokemon/Main.scala
@@ -0,0 +1,7 @@
+package pokemon
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+}
